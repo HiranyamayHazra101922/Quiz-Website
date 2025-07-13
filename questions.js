2f0718ce56d01b71870e54b593b806437d4de90c
HiranyamayHazra101922 <hiranmoyhazra22@gmail.com>
2025-07-13 22:27:11 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,67 @@
+let questions = [{
+        numb: 1,
+        question: "What is 2 + 2 ?",
+        answer: "(B) 4",
+        options: [
+            "(A) 3",
+            "(B) 4",
+            "(C) 1",
+            "(D) 0"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is 10 - 2 ?",
+        answer: "(A) 8",
+        options: [
+            "(A) 8",
+            "(B) 10",
+            "(C) 12",
+            "(D) 9"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is 2 x 10 ?",
+        answer: "(D) 20",
+        options: [
+            "(A) 21",
+            "(B) 14",
+            "(C) 15",
+            "(D) 20"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is 50 % 5 ?",
+        answer: "(B) 10",
+        options: [
+            "(A) 25",
+            "(B) 10",
+            "(C) 50",
+            "(D) 5"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is (10 + 20) % 15 ?",
+        answer: "(A) 2",
+        options: [
+            "(A) 2",
+            "(B) 10",
+            "(C) 20",
+            "(D) 5"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is 81 % 9 ?",
+        answer: "(C) 9",
+        options: [
+            "(A) 20",
+            "(B) 18",
+            "(C) 9",
+            "(D) 12"
+        ]
+    }
+];
